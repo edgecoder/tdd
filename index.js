14444c7120bad88fe7d8abece84bf4d8d92b554c
Son Truong <sont85@gmail.com>
2015-06-11 15:08:25 -0700

    merge

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var doubler = function(a, b) {
-<<<<<<< HEAD
-	return (a * 2) + (b*2)
-=======
   return 2 * a + 2 * b;
->>>>>>> 76d7cc298de71b7fe2e0dc4fa35830d61ce54a3b
 }
 
 console.assert(doubler(4,2) === 12);
