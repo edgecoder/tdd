434ad547c8b6d30c3a1fd233baf458a59a8a2ffa
Michael Sankovich <misankovich@gmail.com>
2015-06-11 14:54:39 -0700

    solved problem

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var doubler = function(a, b) {
+  var double = (a + b)*2
+  return double;
 }
-
 console.assert(doubler(4,2) === 12);
 console.assert(doubler(1,3) === 8);
 console.assert(doubler(-1,3) === 4);
