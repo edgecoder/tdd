5cbe2d2e1ffcb0b60002ef65cd6bc96239bbd77d
warlyware <danward@gmail.com>
2015-06-11 15:14:17 -0700

    doubler number 2

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var doubler = function(a, b) {
-	var result = ((a*2) + (b*2));
-	return result;
+	if (Number.isInteger(a) && Number.isInteger(b)) {
+	  return (2 * a) + (2 * b);
+	} else {
+		String(a);
+		String(b);
+		return (a + a) + (b + b);	
+	}
+	console.log(a, b);
 }
 
 console.assert(doubler(4,2) === 12);
 console.assert(doubler(1,3) === 8);
 console.assert(doubler(-1,3) === 4);
+
+console.assert(doubler("a", "b") === "aabb");
+console.assert(doubler("coding", "rocks") === "codingcodingrocksrocks");
