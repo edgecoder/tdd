7d4622f9fc899b95e9105845a6dffb5dfaa9a7a2
Troy S. Wood <tsw@outlook.com>
2015-06-11 17:42:30 -0700

    troys suggestion

---
+++ troy.js
@@ -0,0 +1,14 @@
+// As per Troy
+//Create a function that will read an object that contains words. The function will receive n args that will be a number. The object will match that number with the matching word with that many letters, resulting in a random sentence.
+
+
+
+var words = { 1: a , 2: my , 3: dog , 4: will , 5: barks , 6: barked}
+
+function createSentence(args){
+
+};
+
+console.assert(createSentence(1, 3, 5) === "my dog barks");
+
+
