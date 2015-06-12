391de6664f7476bfd5e17a8cb539a026e9fd496a
cnichols <mellowdious@mail.com>
2015-06-11 17:28:49 -0700

    operate()

---
+++ cade.js
@@ -0,0 +1,6 @@
+var operate(){}
+
+console.assert(operate('+')(2,2) === 4);
+console.assert(operate('-')(5,4) === 1);
+console.assert(operate('*')(4,3) === 12);
+console.assert(operate('/')(4,2) === 2);
