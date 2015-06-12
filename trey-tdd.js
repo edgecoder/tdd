aa7566d49d941f924835d137f8d0fba4b04598ae
Trey Huffine <treyhuffine@gmail.com>
2015-06-11 17:28:29 -0700

    Create a challenge for Samer

---
+++ trey-tdd.js
@@ -0,0 +1,11 @@
+// Print out any number in words
+//  100 = "one hundred"
+//  10 = "ten"
+//  2500052 = "two million five hundred thousand fifty two"
+//
+
+function numToWords(num) {}
+
+console.assert(numToWords(100), "one hundred");
+console.assert(numToWords(10), "ten");
+console.assert(numToWords(100), "two million five hundred thousand fifty two");
