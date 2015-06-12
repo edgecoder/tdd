655585495b0a87dd1b070e35cab586fa75c51746
Collin Webb <collindwebb@gmail.com>
2015-06-11 17:35:58 -0700

    fixed prime

--- Collin.js
+++ Collin.js
@@ -2,6 +2,6 @@ var isPrime = function () {
 
 };
 
-console.assert(isPrime(6987) === true);
+console.assert(isPrime(131071) === true);
 console.assert(isPrime(6345267) === false);
-console.assert(isPrime(257885161) === true);
+console.assert(isPrime(999999000001) === true);
