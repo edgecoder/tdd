8e5ac62f1a62326e2bc1b343f46aa0db26c682ff
skl92 <stanleykliu92@gmail.com>
2015-06-11 17:32:42 -0700

    stan challenge

---
+++ createChallenge.js
@@ -0,0 +1,9 @@
+// Stanley Javascript Challenge
+
+var mergeArraySort = function() {
+
+}
+
+// given any number of arrays, sort all the numbers in the arrays
+console.assert(mergeArraySort([1,4,6,10],[2,2,3],[9,14,15,1,9]),[1,1,2,2,3,4,6,9,9,10,14,15]);
+console.assert(mergeArraySort([1,2],[4,5],[9,9],[3,2]),[1,2,2,3,4,5,9,9]);
